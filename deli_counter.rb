16df9f2526ec7983cdeee58bbd88901b6a1004ce
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-23 23:02:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def line(arr)
+  if arr.length == 0 
+    puts "The line is currently empty."
+  end
+
+  namesAndNumbers = []
+  i = 0
+
+  while i < arr.length
+  	namesAndNumbers << "#{i.to_i + 1}. #{arr[i]}"
+  	i += 1
+  end
+  puts "The line is currently: #{namesAndNumbers.join(", ")}"
+
+
+def take_a_number(arr, name)
+  arr.push(name)
+  puts "Welcome, #{name}. You are number #{arr.length} in line."
+end
+
+def now_serving(arr)
+  if arr.length == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{arr.shift()}."
+  end
+end
+
